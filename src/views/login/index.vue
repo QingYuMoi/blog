568276748c907bfb39c12ba3789b58afac3d8756
QingYuMoi <1352595340@qq.com>
2021-03-16 17:23:10 +0800

    项目构建,全局配置,路由,elementUI

---
+++ src/views/login/index.vue
@@ -0,0 +1,14 @@
+<template>
+   <div id="login">
+      登录页面
+   </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+   #login{
+      height: 100vh;
+      background-color: #353;
+   }
+</style>
